47f0fc9d432a0eeeec80793b8251ea322e713340
Jayphen <jayphen@gmail.com>
2017-09-20 01:36:31 +0200

    Add drafts to Dato

--- dato.config.js
+++ dato.config.js
@@ -1,14 +1,15 @@
 module.exports = (dato, root, i18n) => {
 
-  // inside a "post" directory...
+  // inside the "post" directory...
   root.directory("site/content/post", (dir) => {
 
-    // ...iterate over the "Blog post" records...
+    // ...iterate over the "post" records...
     dato.posts.forEach(post => {
 
       // ...and create a markdown file for each article!
       dir.createPost(`${post.slug}.md`, "yaml", {
         frontmatter: {
+          draft: post.draft,
           title: post.title,
           subtitle: post.subtitle,
           author: post.author.firstName,
