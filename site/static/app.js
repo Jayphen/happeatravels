6e6cfb6f74137b33bed270a5031b6fc4f82409f0
Jayphen <jayphen@gmail.com>
2017-09-27 01:02:30 +0200

    Keyboard shortcuts in gallery

--- site/static/app.js
+++ site/static/app.js
@@ -610,19 +610,68 @@ var toggleLightbox = function toggler(item) {
   item.target.classList.toggle('lightbox--opened');
 };
 
+var addLightboxClass = function addLightboxClass(item) {
+  if (item.classList) {
+    item.classList.add('lightbox--closed');
+  } else {
+    item.className += ' lightbox--closed';
+  }
+};
+
+var closeOnEsc = function closeOnEsc(event) {
+  if (event.keyCode === 27) {
+    var openLightboxes = document.querySelector('.lightbox--opened');
+
+    if (openLightboxes) {
+      openLightboxes.classList.toggle('lightbox--opened');
+    }
+  }
+};
+
+var navOnArrowKey = function navOnArrowKey(event) {
+  var currentlyActive = document.querySelector('.lightbox--opened');
+  var previousEl = [];
+
+  if (event.keyCode === 37) {
+    if (currentlyActive.parentNode.previousElementSibling) {
+      previousEl = currentlyActive.parentNode.previousElementSibling.querySelector('.lightbox--closed');
+    } else {
+      previousEl = currentlyActive.parentNode.parentNode.querySelectorAll('.lightbox--closed');
+      previousEl = previousEl[previousEl.length - 1];
+    }
+    currentlyActive.classList.toggle('lightbox--opened');
+    previousEl.classList.toggle('lightbox--opened');
+  }
+};
+
 var initLightbox = function init(className) {
   var lightboxImage = document.querySelectorAll(className);
   lightboxImage.forEach(function (item) {
-    if (item.classList) {
-      item.classList.add('lightbox--closed');
-    } else {
-      item.className += ' lightbox--closed';
-    }
+    addLightboxClass(item);
     item.addEventListener('click', toggleLightbox);
   });
+
+  document.addEventListener('keyup', closeOnEsc);
+  document.addEventListener('keyup', navOnArrowKey);
 };
 
 module.exports = initLightbox;
+
+// $(document).on('keyup', function(event) {
+//   if ($('img.lightbox--opened').length !== 0) {
+//     if (event.keyCode === 27) {
+//       $('.lightbox--opened').removeClass('lightbox--opened');
+//       $(document).off('touchmove');
+//     } else if (event.keyCode === 37 || event.keyCode === 39) {
+//       var elements = $('img.lightbox');
+//       var element = $(elements[elements.index($('img.lightbox--opened')) + (event.keyCode === 37 ? -1 : +1)]);
+//       if (element.length !== 0) {
+//         elements.removeClass('lightbox--opened')
+//         element.addClass('lightbox--opened')
+//       }
+//     }
+//   }
+// });
 
 /***/ })
 /******/ ]);
