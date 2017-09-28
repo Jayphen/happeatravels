8e7f9fd69327b49d15a28079cfdc319485563028
Jayphen <jayphen@gmail.com>
2017-09-29 00:26:03 +0200

    Move post content into .Content instead of .Params. This enables shortcodes in the CMS

--- dato.config.js
+++ dato.config.js
@@ -1,4 +1,5 @@
 const omitDeep = require('omit-deep');
+const outdent = require('outdent');
 
 module.exports = (dato, root, i18n) => {
 
@@ -8,9 +9,34 @@ module.exports = (dato, root, i18n) => {
     // ...iterate over the "post" records...
     dato.posts.forEach(post => {
 
-      const onlyContent = [post.introBlock, post.contentContainer.toMap().map(container => container.content)].join('\n\n');
-
       const contentWithoutSEO = omitDeep(post.contentContainer.toMap(), ['seoMetaTags']);
+
+      let content = [];
+
+      post.contentContainer.toMap().forEach((val, index) => {
+        switch (val.itemType) {
+          case "text_block":
+            content.push(`{{< text_block >}}${val.content}{{< /text_block >}}`);
+            break;
+          case "single_image":
+            content.push(outdent`
+              {{% single_image url="${val.image.url}" caption="${val.caption ? val.caption : null}" alt="${val.image.alt === null ? '' : val.image.alt}" %}}
+            `)
+            break;
+          case "gallery":
+            // For each gallery item, create a string of values delimited by commas
+            // put the strings in an array
+            // Join the array by wrapping the strings in quotes
+            let galleryItems = [];
+            val.gallery.forEach(galleryItem => {
+              galleryItems.push(`${galleryItem.height},${galleryItem.width},${galleryItem.url},${galleryItem.alt},${galleryItem.title}`);             
+            })
+            let items = galleryItems.join('" "');
+            content.push(outdent`
+              {{% gallery "${val.galleryType.galleryType}" "${items}" %}}
+            `)
+        }
+      });
 
       // ...and create a markdown file for each article!
       dir.createPost(`${post.slug}.md`, "yaml", {
@@ -28,15 +54,12 @@ module.exports = (dato, root, i18n) => {
           country: {
               name: post.country.name,
               code: post.country.countryCode,
-              continent: post.country.continent.name
           },
           destination: post.country.continent.name,
-          feature_image: post.featureImage.url(),
           feature_image_data: post.featureImage.toMap(),
-          intro: post.introBlock,
-          content: contentWithoutSEO
+          intro: post.introBlock
         },
-        content: onlyContent
+        content: content.join('\n')
       });
     });
   });
